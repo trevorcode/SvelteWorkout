9d9e4cdfafac03ef82704e5579da3dfa9f8005b9
Trevor <trevorcode@outlook.com>
2021-08-10 20:01:33 -0600

    Setup gh pages

---
+++ my-svelte-project/gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/trevorcode/SvelteWorkout.git', // Update to point to your repository
+		user: {
+			name: 'Trevor', // update to use your name
+			email: '' // Update to use your email
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
